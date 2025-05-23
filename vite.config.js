83e83eb9753f6c21fa904784564f51c191f3e4ff
Ms-Eve <mushunjer@yahoo.com>
2025-05-23 13:04:39 +0200

    adding files

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,21 @@ export default defineConfig({
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
+            publicDir: 'public',
+
+            // Optional but recommended for complex projects
+            //   This gives you more control over the input files
+            //   and how they're bundled.
+            build: {
+                rollupOptions: {
+                    input: {
+                        // e.g. multiple entry points
+                        main: 'resources/js/app.js',
+                        vendor: 'resources/js/vendor.js',
+                    },
+                },
+            },
         }),
     ],
 });
+
